44d6240a229cd5ae7d4ddff368d294ee4fcef019
manivel-sellamuthu <manivel-sellamuthu@users.noreply.github.com>
2019-11-28 19:16:41 +0530

    Export grid as images sample added

---
+++ ExportAsImage_Core/Program.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.EJ2.PdfViewer;
+using System;
+using System.Drawing;
+
+namespace ExportAsImage_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Uses the Syncfusion.EJ2.PdfViewer assembly
+            PdfRenderer pdfExportImage = new PdfRenderer();
+            pdfExportImage.Load(@"currentDirectory\..\..\..\..\Data\pdfdocument.pdf");
+            Bitmap[] bitmapimage = pdfExportImage.ExportAsImage(0, pdfExportImage.PageCount-1);
+            for(int i =0; i<bitmapimage.Length;i++)
+            {
+                bitmapimage[i].Save(@"currentDirectory\..\..\..\..\Images\" + "bitmapImage" + i.ToString() + ".png");
+               
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
